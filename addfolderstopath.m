37beaa2576c729a115302e3024239518c06591ec
Srividya Prasad <srividyaprasad3@gmail.com>
2023-09-10 23:49:12 +0530

    Added file which adds Matlab to folder path

---
+++ addfolderstopath.m
@@ -0,0 +1,11 @@
+% Replace 'your_directory_path' with the path to the directory you want to add
+directory_path = 'C:\Users\srivi\OneDrive\Desktop\IISc\MSS';
+
+% Use the 'genpath' function to generate a list of 'directory_path' and all its subdirectories
+all_subdirectories = genpath(directory_path);
+
+% Add all the subdirectories to the MATLAB search path
+addpath(all_subdirectories);
+
+% To save this path for future MATLAB sessions, use 'savepath'
+savepath;
